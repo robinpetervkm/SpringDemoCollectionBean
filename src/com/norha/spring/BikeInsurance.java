e622f4b074d5a6c74f29f47c55d1f6b1285e09eb
Robinclear <robin.peter.vkm@gmail.com>
2020-04-17 00:15:59 -0300

    List injected to the beans

--- src/com/norha/spring/BikeInsurance.java
+++ src/com/norha/spring/BikeInsurance.java
@@ -1,7 +1,15 @@
 package com.norha.spring;
 
+import java.util.List;
+
 public class BikeInsurance implements Insurance {
 	
+	public BikeInsurance(List<String> members) {
+		System.out.println("LIST OF MEMBERS");
+		for (String member : members) {
+			System.out.println(member);
+		}
+	}
 
 	@Override
 	public String showStatus() {
