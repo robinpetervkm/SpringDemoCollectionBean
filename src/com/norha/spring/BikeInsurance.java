dc5145559d9dec000a763558761733e84ac1238d
Robinclear <robin.peter.vkm@gmail.com>
2020-04-17 02:11:32 -0300

    INjuction is not happening becouse of removeing the Constructor-arg

--- src/com/norha/spring/BikeInsurance.java
+++ src/com/norha/spring/BikeInsurance.java
@@ -2,10 +2,8 @@ package com.norha.spring;
 
 
 public class BikeInsurance implements Insurance {
-	private Offers offers;
 	public BikeInsurance(Offers o) {
-		this.offers=o;
-		System.out.println(this.offers.getOffer());
+		System.out.println(o.getOffer());
 	}
 
 	@Override
